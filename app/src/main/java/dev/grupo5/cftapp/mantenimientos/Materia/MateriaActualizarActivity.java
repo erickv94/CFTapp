e43b06eb594bd7e2b750346e38ba5c83444d56f9
vanessacastro <vanessa-castro30@hotmail.com>
2019-05-15 13:14:24 -0600

    mantenimiento materia

--- app/src/main/java/dev/grupo5/cftapp/mantenimientos/Materia/MateriaActualizarActivity.java
+++ app/src/main/java/dev/grupo5/cftapp/mantenimientos/Materia/MateriaActualizarActivity.java
@@ -2,10 +2,19 @@ package dev.grupo5.cftapp.mantenimientos.Materia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import dev.grupo5.cftapp.R;
+import dev.grupo5.cftapp.database.MateriaDB;
+import dev.grupo5.cftapp.modelos.Materia;
 
 public class MateriaActualizarActivity extends AppCompatActivity {
+    MateriaDB materiaDB;
+    EditText codigomateriaText;
+    EditText nombremateriaText;
+    EditText uvsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,26 @@ public class MateriaActualizarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_materia_actualizar);
         setTitle(R.string.materiaupdate);
 
+        codigomateriaText = (EditText) findViewById(R.id.codigomateria);
+        nombremateriaText = (EditText) findViewById(R.id.nombremateria);
+        uvsText = (EditText) findViewById(R.id.uvs);
+    }
+
+    public void actualizarMateria (View v){
+        Materia materia = new Materia();
+        materiaDB = new MateriaDB(this);
+        String resultado;
+        materia.setCodigoMateria(codigomateriaText.getText().toString());
+        materia.setNombre(nombremateriaText.getText().toString());
+        materia.setUvs(Integer.parseInt(uvsText.getText().toString()));
+
+        resultado = materiaDB.actualizar(materia);
+        Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarMateria(View v){
+        codigomateriaText.setText("");
+        nombremateriaText.setText("");
+        uvsText.setText("");
     }
 }
--- app/src/main/java/dev/grupo5/cftapp/mantenimientos/Materia/MateriaConsultarActivity.java
+++ app/src/main/java/dev/grupo5/cftapp/mantenimientos/Materia/MateriaConsultarActivity.java
@@ -2,10 +2,20 @@ package dev.grupo5.cftapp.mantenimientos.Materia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import dev.grupo5.cftapp.R;
+import dev.grupo5.cftapp.database.MateriaDB;
+import dev.grupo5.cftapp.modelos.Materia;
 
 public class MateriaConsultarActivity extends AppCompatActivity {
+    MateriaDB materiaDB;
+    EditText idmateriaText;
+    EditText nombremateriaText;
+    EditText codigomateriaText;
+    EditText uvsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,30 @@ public class MateriaConsultarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_materia_consultar);
         setTitle(R.string.materiaread);
 
+        idmateriaText = (EditText) findViewById(R.id.idbusqueda);
+        nombremateriaText = (EditText) findViewById(R.id.nombremateria);
+        codigomateriaText = (EditText) findViewById(R.id.codigomateria);
+        uvsText = (EditText) findViewById(R.id.uvs);
+    }
+
+    public void consultarMateria(View v){
+        MateriaDB materiaDB = new MateriaDB(this);
+        Materia materia;
+        materia = materiaDB.consultar(idmateriaText.getText().toString());
+        if (materia != null){
+            materia.setCodigoMateria(codigomateriaText.getText().toString());
+            materia.setNombre(nombremateriaText.getText().toString());
+            materia.setUvs(Integer.parseInt(uvsText.getText().toString()));
+            return;
+        }
+        Toast.makeText(this, "Materia con id materia" + idmateriaText.getText().toString()
+        + "no existe", Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarMateria(View v){
+        idmateriaText.setText("");
+        nombremateriaText.setText("");
+        codigomateriaText.setText("");
+        uvsText.setText("");
     }
 }
