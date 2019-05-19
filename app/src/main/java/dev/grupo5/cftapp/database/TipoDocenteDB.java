a32831538699d1b1e68ea4be95d416668a4e3f1e
12Neto <1294neto@gmail.com>
2019-05-19 12:43:03 -0600

    Update TipoDocenteDB.java

--- app/src/main/java/dev/grupo5/cftapp/database/TipoDocenteDB.java
+++ app/src/main/java/dev/grupo5/cftapp/database/TipoDocenteDB.java
@@ -11,8 +11,7 @@ import dev.grupo5.cftapp.modelos.TipoDocente;
 
 
 public class TipoDocenteDB {
-    private static final String[]camposTipoDocente = new String []
-            {"idTipoDocente","nombre"};
+    private String[]camposTipoDocente = {"idTipoDocente","nombre"};
 
     private SQLiteDatabase db;
     private DBHelper dbHelper;
@@ -23,37 +22,6 @@ public class TipoDocenteDB {
         dbHelper=DBHelper.getSingleton(context);
     }
 
-    private static class DatabaseHelper extends SQLiteOpenHelper {
-        private static final String BASE_DATOS = "proyecto1.s3db";
-        private static final int VERSION = 1;
-
-        public DatabaseHelper(Context context) {
-            super(context, BASE_DATOS, null, VERSION);
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            try{
-                db.execSQL("CREATE TABLE tipodocente(idtipodocente integer(7) NOT NULL PRIMARY KEY,nombre VARCHAR(30));");
-            }catch(SQLException e){
-                e.printStackTrace();
-            }
-        }
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-// TODO Auto-generated method stub
-        }
-    }
-    public void abrir() throws SQLException{
-        db = dbHelper.getWritableDatabase();
-        return;
-    }
-    public void cerrar(){
-        dbHelper.close();
-    }
-
-
-
-
     public String insertar(TipoDocente tipoDocente){
         String regInsertados="Registros Insertados No: ";
         long contador=0;
@@ -61,14 +29,10 @@ public class TipoDocenteDB {
         ContentValues contentValues= new ContentValues();
         contentValues.put("nombre",tipoDocente.getNombre());
 
+        db=dbHelper.getWritableDatabase();
         contador=db.insert("tipodocente",null,contentValues);
-        if(contador==-1 || contador==0)
-        {
-            regInsertados= "Error al Insertar el registro, Registro Duplicado. Verificar inserción";
-        }
-        else {
-            regInsertados=regInsertados+contador;
-        }
+        regInsertados=regInsertados+contador;
+
         return regInsertados;
 
 
