3b5cc42a7bd45fdd4a759b04c16165dd73b2b9d5
ElmerHernandez <elmerh49@gmail.com>
2019-06-14 23:32:52 -0600

    InsertarLocalWebService

---
+++ app/src/main/java/dev/grupo5/cftapp/databaseWS/LocalWS.java
@@ -0,0 +1,61 @@
+package dev.grupo5.cftapp.databaseWS;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+
+public class LocalWS {
+
+    public static String obtenerRespuestaPeticion(String url, Context ctx) {
+
+        String respuesta = " ";
+
+        // Estableciendo tiempo de espera del servicio
+        HttpParams parametros = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(parametros, 3000);
+        HttpConnectionParams.setSoTimeout(parametros, 5000);
+
+        // Creando objetos de conexion
+        HttpClient cliente = new DefaultHttpClient(parametros);
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse httpRespuesta = cliente.execute(httpGet);
+            StatusLine estado = httpRespuesta.getStatusLine();
+            int codigoEstado = estado.getStatusCode();
+            if (codigoEstado == 200) {
+                HttpEntity entidad = httpRespuesta.getEntity();
+                respuesta = EntityUtils.toString(entidad);
+            }
+        } catch (Exception e) {
+            Toast.makeText(ctx, "Error en la conexion", Toast.LENGTH_LONG)
+                    .show();
+            // Desplegando el error en el LogCat
+            Log.v("Error de Conexion", e.toString());
+        }
+        return respuesta;
+    }
+
+
+    public static String InsertarLocalServidor(String peticion, Context ctx){
+        String json=obtenerRespuestaPeticion(peticion,ctx);
+        String [] resultado = json.split(";");
+        if(resultado[1].equals("{\"resultado\":1}")){
+            return "LOCAL INSERTADO CON EXITO";
+        }
+        else {
+            return "LOCAL NO INSERTADO";
+        }
+    }
+
+}
--- app/src/main/java/dev/grupo5/cftapp/mantenimientos/Localwbs/LocalInsertarWbsActivity.java
+++ app/src/main/java/dev/grupo5/cftapp/mantenimientos/Localwbs/LocalInsertarWbsActivity.java
@@ -1,16 +1,66 @@
 package dev.grupo5.cftapp.mantenimientos.Localwbs;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import dev.grupo5.cftapp.R;
+import dev.grupo5.cftapp.authguard.Auth;
+import dev.grupo5.cftapp.databaseWS.LocalWS;
 
+@SuppressLint("NewApi")
 public class LocalInsertarWbsActivity extends AppCompatActivity {
-
+    EditText codigoEdificioText;
+    EditText nombreText;
+    EditText codigoLocalText;
+    EditText capacidadText;
+    private static final int  permiso=17;
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_insertar_wbs);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setTitle(getResources().getString(R.string.localinsert)+" webservice");
+        verificarPermisos();
+        codigoEdificioText= findViewById(R.id.codigoedificio);
+        nombreText= findViewById(R.id.nombrelocal);
+        codigoLocalText= findViewById(R.id.codigolocal);
+        capacidadText=findViewById(R.id.capacidad);
     }
+
+    public void verificarPermisos(){
+
+        if(!Auth.userHasPermission(Auth.guard(this),this,permiso)){
+            finish();
+            Toast.makeText(this,getResources().getString(R.string.no_permisos)+" "
+                    +getResources().getString(R.string.localinsert), Toast.LENGTH_LONG).show();
+        }
+
+    }
+    public void limpiar(View view){
+        codigoLocalText.setText("");
+        codigoEdificioText.setText("");
+        nombreText.setText("");
+        capacidadText.setText("");
+    }
+
+    public void insertarLocalServidor(View v){
+        String url;
+        String mensaje;
+        url="http://192.168.1.89:8080/ws_local_insert.php?" +
+                "codigoedificio=" +codigoEdificioText.getText().toString()+
+                "&nombrelocal=" +nombreText.getText().toString()+
+                "&codigolocal=" +codigoLocalText.getText().toString()+
+                "&capacidad="+capacidadText.getText().toString();
+
+        mensaje= LocalWS.InsertarLocalServidor(url,this);
+        Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show();
+    }
+
 }
